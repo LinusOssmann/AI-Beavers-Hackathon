6374d4dba8e375a069ba942b2627faca871686d9
Linus Ossmann <linus.ossmann@gmail.com>
2026-02-01 11:41:01 +0100

    configure postinstall & turbo config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+	experimental: {
+		turbo: {
+			resolveAlias: {
+				"@": "./",
+				"@/generated": "./generated",
+			},
+		},
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	images: {
+		unoptimized: true,
+	},
+};
 
-export default nextConfig
+export default nextConfig;
