e334fe6a2cb2258d865c0aa0f4bb98d443e85616
Linus Ossmann <linus.ossmann@gmail.com>
2026-02-01 12:16:32 +0100

    pass better auth env

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,6 +8,8 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL,
   advanced: { disableOriginCheck: true },
   database: prismaAdapter(prisma, {
     provider: "postgresql",
